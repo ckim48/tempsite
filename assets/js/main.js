1148c8842cc0ff9ebf2406a5bf62b1b25e2fbd9a
ckim48 <ckim48@u.rochester.edu>
2023-03-10 09:16:19 +0900

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -277,7 +277,141 @@
     });
   }
 
+const spyEls = document.querySelectorAll('.scroll-spy')
+spyEls.forEach(function(spyEl){
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
+})
 
+var options = {
+          series: [
+          {
+            name: 'Actual',
+            data: [
+              {
+                x: '2011',
+                y: 12,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 14,
+                    strokeWidth: 2,
+                    strokeDashArray: 2,
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              },
+              {
+                x: '2012',
+                y: 44,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 54,
+                    strokeWidth: 5,
+                    strokeHeight: 10,
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              },
+              {
+                x: '2013',
+                y: 54,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 52,
+                    strokeWidth: 10,
+                    strokeHeight: 0,
+                    strokeLineCap: 'round',
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              },
+              {
+                x: '2014',
+                y: 66,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 61,
+                    strokeWidth: 10,
+                    strokeHeight: 0,
+                    strokeLineCap: 'round',
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              },
+              {
+                x: '2015',
+                y: 81,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 66,
+                    strokeWidth: 10,
+                    strokeHeight: 0,
+                    strokeLineCap: 'round',
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              },
+              {
+                x: '2016',
+                y: 67,
+                goals: [
+                  {
+                    name: 'Expected',
+                    value: 70,
+                    strokeWidth: 5,
+                    strokeHeight: 10,
+                    strokeColor: '#775DD0'
+                  }
+                ]
+              }
+            ]
+          }
+        ],
+          chart: {
+          height: 350,
+          type: 'bar'
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+          }
+        },
+        colors: ['#00E396'],
+        dataLabels: {
+          formatter: function(val, opt) {
+            const goals =
+              opt.w.config.series[opt.seriesIndex].data[opt.dataPointIndex]
+                .goals
+        
+            if (goals && goals.length) {
+              return `${val} / ${goals[0].value}`
+            }
+            return val
+          }
+        },
+        legend: {
+          show: true,
+          showForSingleSeries: true,
+          customLegendItems: ['Actual', 'Expected'],
+          markers: {
+            fillColors: ['#00E396', '#775DD0']
+          }
+        }
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
+      
   /**
    * Initiate Pure Counter 
    */
